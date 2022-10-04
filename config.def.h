227b29575f87f050f014e2a65d9c78d4cef08aee
Shashwat Sarkar <shashwatsarkar22@gmail.com>
2022-10-04 13:40:24 +0530

    increased number of lines & columns

--- config.def.h
+++ config.def.h
@@ -14,8 +14,8 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 3;
-static unsigned int columns    = 8;
+static unsigned int lines      = 4;
+static unsigned int columns    = 9;
 
 /*
  * Characters not considered part of a word while deleting words
